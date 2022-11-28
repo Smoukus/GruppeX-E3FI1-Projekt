a180114da60532df6e77cbace1498b1816de0800
wtom1 <wtom1731@gmail.com>
2022-11-28 21:01:07 +0100

    Issue fixed

--- Systemintegrator.py
+++ Systemintegrator.py
@@ -1,9 +1,10 @@
 class Systemintegrator:
 
-    def __init__(self, name, alter, jahreErfahrung):
+    def __init__(self, name, alter, jahreErfahrung, firma):
         self.__name = name
         self.__alter = alter
         self.__jahreErfahrung = jahreErfahrung
+        self.__firma = firma
 
     def getName(self):
         return self.__name
@@ -23,5 +24,11 @@ class Systemintegrator:
     def setJahreErfahrung(self, jahreErfahrung):
         self.__jahreErfahrung = jahreErfahrung
 
+    def getFirma(self):
+        return self.__firma
+
+    def setFirma(self, firma):
+        self.__firma = firma
+
     def __str__(self) -> str:
-        return f"Name: {self.__name}, Alter: {self.__alter}, Jahre Erfahrung: {self.__jahreErfahrung}"
+        return f"Name: {self.__name}, Alter: {self.__alter}, Jahre Erfahrung: {self.__jahreErfahrung}, Firma: {self.__firma}"
