a073dfd44d8c50121ad29fe8adf561000ccea172
wtom1 <wtom1731@gmail.com>
2022-11-28 20:52:45 +0100

    -.-

--- Systemintegrator.py
+++ Systemintegrator.py
@@ -1,28 +1,27 @@
 class Systemintegrator:
 
     def __init__(self, name, alter, jahreErfahrung):
-        self.name = name
-        self.alter = alter
-        self.jahreErfahrung = jahreErfahrung
+        self.__name = name
+        self.__alter = alter
+        self.__jahreErfahrung = jahreErfahrung
 
     def getName(self):
-        return self.name
+        return self.__name
 
     def setName(self, name):
-        self.name = name
+        self.__name = name
     
     def getAlter(self):
-        return self.alter
+        return self.__alter
 
     def setAlter(self, alter):
-        self.alter = alter
+        self.__alter = alter
 
     def getJahreErfahrung(self):
-        return self.jahreErfahrung
+        return self.__jahreErfahrung
 
     def setJahreErfahrung(self, jahreErfahrung):
-        self.jahreErfahrung = jahreErfahrung
+        self.__jahreErfahrung = jahreErfahrung
 
     def __str__(self) -> str:
-        return f"Name: {self.name}, Alter: {self.alter}, Jahre Erfahrung: {self.getJahreErfahrung}"
-
+        return f"Name: {self.__name}, Alter: {self.__alter}, Jahre Erfahrung: {self.__jahreErfahrung}"
